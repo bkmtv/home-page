e4d9eb695c3bad3c773d731a7f12f8e228fc169f
Yuri Bekmetov <bekm.yu@yandex.ru>
2022-11-12 15:57:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// window.onload = function () {
-//     function random () {
-//         return Math.floor(Math.random() * 256)
-//     }
-//     function setBg () {
-//         document.body.style.backgroundColor =
-//         "rgb(" + random () + "," + random () + "," + random () + ")";
-//     }
-//     setBg ()
-// }
-
 function toggle() {
     var element = document.body;
     element.classList.toggle("dark-mode");
