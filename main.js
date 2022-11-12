381c0234e5271267afd058a4b7464c760d507f03
Yuri Bekmetov <bekm.yu@yandex.ru>
2022-11-12 15:54:49 +0200

    big update
    added toggle for dark mode, new info blocks and links for social.

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-window.onload = function () {
-    function random () {
-        return Math.floor(Math.random() * 256)
-    }
-    function setBg () {
-        document.body.style.backgroundColor =
-        "rgb(" + random () + "," + random () + "," + random () + ")";
-    }
-    setBg ()
-}
+// window.onload = function () {
+//     function random () {
+//         return Math.floor(Math.random() * 256)
+//     }
+//     function setBg () {
+//         document.body.style.backgroundColor =
+//         "rgb(" + random () + "," + random () + "," + random () + ")";
+//     }
+//     setBg ()
+// }
+
+function toggle() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
